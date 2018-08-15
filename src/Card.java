449fd8403cef015532f54e441285c5849f6d5b91
andrepet <pettersson.andreas.lennart@gmail.com>
2018-08-15 17:33:13 +0200

    Add hidden and flip feature to card

--- src/Card.java
+++ src/Card.java
@@ -2,6 +2,7 @@ public class Card {
 
     private final Color color;
     private final Value value;
+    private boolean hidden = false;
 
     public Card(Color color, Value value) {
         this.color = color;
@@ -14,6 +15,21 @@ public class Card {
 
     public Value getValue() {
         return value;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void flip() {
+        this.hidden = !this.hidden;
+    }
+
+    public String display(){
+        if (hidden)
+            return "HIDDEN";
+        else
+            return this.color + " " + this.value;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Card myCard = new Card(Color.HEARTS, Value.SEVEN);
         System.out.println(myCard);
+        System.out.println(myCard.display());
+        System.out.println(myCard.isHidden());
+        myCard.flip();
+        System.out.println(myCard.display());
+        System.out.println(myCard.isHidden());
+        myCard.flip();
+        System.out.println(myCard.display());
+        System.out.println(myCard.isHidden());
     }
 }
