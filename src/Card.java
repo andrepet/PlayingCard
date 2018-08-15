4662c4c9f5f3d3a3c079bfbb119a39b5ce8f8c68
andrepet <pettersson.andreas.lennart@gmail.com>
2018-08-15 17:22:49 +0200

    Add basic playing card

---
+++ src/Card.java
@@ -0,0 +1,26 @@
+public class Card {
+
+    private final Color color;
+    private final Value value;
+
+    public Card(Color color, Value value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "color=" + color +
+                ", value=" + value +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("chokladkaka");
+        Card myCard = new Card(Color.HEARTS, Value.SEVEN);
+        System.out.println(myCard);
     }
 }
